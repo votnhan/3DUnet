76704e25506cd0cf39a5c4420c1154c945b3c893
trungnhanuchiha <33752801+trungnhanuchiha@users.noreply.github.com>
2019-07-07 18:03:20 +0700

    edit code 18

--- unet3d/training.py
+++ unet3d/training.py
@@ -81,7 +81,7 @@ def train_model(model, model_file, training_generator, validation_generator, ste
                         validation_data=validation_generator,
                         validation_steps=validation_steps,
                         verbose=1,
-                        use_multiprocessing=True,
+                        workers=0,
                         callbacks=get_callbacks(model_file,
                                                 initial_learning_rate=initial_learning_rate,
                                                 learning_rate_drop=learning_rate_drop,
