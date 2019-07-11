54f8c6eb4aaa317d81644fcfd27a274c5aa866f7
trungnhanuchiha <33752801+trungnhanuchiha@users.noreply.github.com>
2019-07-11 23:48:11 +0700

    edit code 25

--- brats/predict.py
+++ brats/predict.py
@@ -10,7 +10,7 @@ from unet3d.prediction import run_validation_cases
 def main():
     prediction_dir = os.path.abspath("prediction")
     run_validation_cases(validation_keys_file=config["validation_file"],
-                         model_file=config["tumor_segmentation_weights.h5"],
+                         model_file=config["weigths_file"],
                          training_modalities=config["training_modalities"],
                          labels=config["labels"],
                          hdf5_file=config["data_file"],
