9acc736c483b703bb290215c5f4d6d1805a0a7f2
trungnhanuchiha <33752801+trungnhanuchiha@users.noreply.github.com>
2019-07-26 21:10:01 +0700

    edit code 66

--- brats/pre_process_BRATS2018.py
+++ brats/pre_process_BRATS2018.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('../')
+
 from brats.preprocess import convert_brats_data
 
 convert_brats_data("brats/data/BRAST2018_not_processed/Train", "brats/data/BRATS2018_preprocessed_cont/Train")
