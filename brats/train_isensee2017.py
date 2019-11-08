7fa136c852b43306eedefef16681406c981480bf
Vo Trung Nhan <1612372@hcmut.edu.vn>
2019-11-08 23:13:33 +0700

    Edit code 84

--- brats/train_isensee2017.py
+++ brats/train_isensee2017.py
@@ -57,7 +57,7 @@ config["overwrite"] = False  # If True, will previous files. If False, will use 
 def fetch_training_data_files(return_subject_ids=False):
     training_data_files = list()
     subject_ids = list()
-    for subject_dir in glob.glob(os.path.join(os.path.dirname(__file__), "data", "BRATS2018_preprocessed_main", "Train", "*", "*")):
+    for subject_dir in glob.glob(os.path.join(os.path.dirname(__file__), "data", "BRATS2018_main", "Train", "*", "*")):
         subject_ids.append(os.path.basename(subject_dir))
         subject_files = list()
         for modality in config["training_modalities"] + ["truth"]:
