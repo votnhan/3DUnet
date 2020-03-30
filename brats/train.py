ce018b2cf8f250f2da7be2fb6681bebb7ae0ef5a
Vo Trung Nhan <1612372@hcmut.edu.vn>
2020-03-31 00:31:43 +0700

    fix bug from imcompatible version

--- brats/train.py
+++ brats/train.py
@@ -13,6 +13,25 @@ from unet3d.training import load_old_model, train_model
 import unet3d.metrics as module_metric
 import keras.optimizers as opts
 
+import tensorflow as tf
+import keras.backend.tensorflow_backend as tfback
+# print("tf.__version__ is", tf.__version__)
+# print("tf.keras.__version__ is:", tf.keras.__version__)
+
+def _get_available_gpus():
+    """Get a list of available gpu devices (formatted as strings).
+
+    # Returns
+        A list of available GPU devices.
+    """
+    #global _LOCAL_DEVICES
+    if tfback._LOCAL_DEVICES is None:
+        devices = tf.config.list_logical_devices()
+        tfback._LOCAL_DEVICES = [x.name for x in devices]
+    return [x for x in tfback._LOCAL_DEVICES if 'device:gpu' in x.lower()]
+
+tfback._get_available_gpus = _get_available_gpus
+
 def fetch_training_data_files(return_subject_ids=False):
     training_data_files = list()
     subject_ids = list()
